e90a81cc31007de1fd8f9738f2d3a3542e2323f6
fierytortilla <dschlessinger@live.com>
2020-05-23 12:47:18 +0100

    started first experiments with integrating P5 sound and visuals into Vue

---
+++ src/components/MusicSettingsController.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:'MusicSettingsController',
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+
+    },
+    methods :{
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
