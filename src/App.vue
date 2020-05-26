4082729efc5cda8dfe1bec73d5cc34cc4af70f3a
fierytortilla <dschlessinger@live.com>
2020-05-26 23:26:26 +0100

    changed logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/driftLogo.png">
     <h1>DRIFT STRUCTURE</h1>
     <music-selection />
     <music-visualization />
